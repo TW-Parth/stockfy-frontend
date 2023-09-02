26fb8ac760eafaf2a75a3bb5334270e77c2058ec
parthpatadiya391 <parth.p@thinkwik.com>
2023-09-02 19:35:37 +0530

    feat - settings API integration

--- src/container/dashboard/Settings.jsx
+++ src/container/dashboard/Settings.jsx
@@ -4,9 +4,15 @@ import ImgFile from '../../assets/images/file-upload.svg';
 import { IconCopy } from '../../component/global/Icons';
 import { SUCCESS_MESSAGE } from '../../helpers/constant';
 import { getWebhookConfig, handleFile } from '../../services/settings/settings.services';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 const Settings = () => {
   const [webhook, setWebhook] = useState();
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   const [uploadingFile, setUploadingFile] = useState(false);
 
   const inputRef = useRef();
@@ -18,6 +24,7 @@ const Settings = () => {
     const { data } = await getWebhookConfig();
     setWebhook({
       url: data?.url,
+      payload: data?.payload,
     })
   }
 
@@ -50,6 +57,55 @@ const Settings = () => {
                 <form>
                   <div className="key-block">
                     <label className="form-label font-700">Web-Hook</label>
+                    <Button variant="primary" onClick={handleShow}>
+                      Launch demo modal
+                    </Button>
+
+                    <Modal show={show} onHide={handleClose}>
+                      <Modal.Header closeButton>
+                        <Modal.Title style={{color:'black'}}>Webhook Integration</Modal.Title>
+                      </Modal.Header>
+                      <Modal.Body style={{color: 'black'}}> <ul><b>UserId: 64f2f2004631e03b53773349 </b>
+                      <b>webhook endpoint: http://192.168.1.190:3001/webhook/ </b>
+                      </ul>
+                      <ul>
+                        <li>You have to invoke this webhook on successful order placed</li>
+                        <li>You need to send following curl request. Which is mentioned below.</li>
+                      </ul>
+                      <div style={{backgroundColor: 'slategrey'}}>{`curl --location '192.168.1.190:3001/webhook/' \
+--form 'userId=""' \
+--form 'orderId=""' \
+--form 'orderDate=""' \
+--form 'shipDate=""' \
+--form 'shipMode=""' \
+--form 'customerId=""' \
+--form 'customerName=""' \
+--form 'segment=""' \
+--form 'country=""' \
+--form 'city=""' \
+--form 'state=""' \
+--form 'postalCode=""' \
+--form 'region=""' \
+--form 'productId=""' \
+--form 'category=""' \
+--form 'subCategory=""' \
+--form 'productName=""' \
+--form 'price=""' \
+--form 'quantity=""' \
+--form 'discount=""' \
+--form 'profit=""' \
+--form 'brandId=""' \
+--form 'brandName=""' \
+--form 'totalProductSells=""' \
+--form 'productRating=""' \
+--form 'stock=""'`}</div>
+                      </Modal.Body>
+                      <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                          Close
+                        </Button>
+                      </Modal.Footer>
+                    </Modal>
                     <div className="api-key">
                       <span>{webhook?.url}</span>
                       <button type="button" className="btn btn-primary">
