587e2862e7c26f3363a74750c0bd243fc9b8b914
PALAK8866 <palakmanek8866@gmail.com>
2023-09-02 12:56:07 +0530

    feat - route privatization

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,29 @@ import HomePage from './container/home/Homepage';
 import Login from './container/auth/Login';
 import Signup from './container/auth/Signup';
 import DashBoardLayout from './container/dashboard/DashboardLayout';
+import { LOCAL_STORAGE_KEYS } from './helpers/contant';
+
+const PublicRoutes = ({children}) => {
+  if(localStorage.getItem(LOCAL_STORAGE_KEYS.ACCESS_TOKEN) === null){
+    return children;
+  }
+  return <Navigate to="/dashboard" replace />
+}
+
+const PrivateRoutes = ({children}) => {
+  if(localStorage.getItem(LOCAL_STORAGE_KEYS.ACCESS_TOKEN) !== null){
+    return children;
+  }
+  return <Navigate to="/" replace />
+}
 
 function App() {
   return (
     <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard/*" element={<DashBoardLayout />} />
+        <Route path="/login" element={<PublicRoutes><Login /></PublicRoutes>} />
+        <Route path="/dashboard/*" element={<PrivateRoutes><DashBoardLayout /></PrivateRoutes>} />
         <Route path="/*" element={<Navigate to="/" replace />} />
         <Route path="/signup" Component={Signup} />
       </Routes>
