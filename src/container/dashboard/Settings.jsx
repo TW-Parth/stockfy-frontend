71cfcc8870556d2ce0562a9ce4a0ae098c47ae18
bhavinmakavana <bhavin@thinkwik.com>
2023-09-02 17:05:41 +0530

    -Setting Page Done

--- src/container/dashboard/Settings.jsx
+++ src/container/dashboard/Settings.jsx
@@ -1,5 +1,48 @@
+import { IconCopy } from '../../component/global/Icons';
+import ImgFile from '../../assets/images/file-upload.svg';
+
 const Settings = () => {
-  return <p>Settings Page</p>;
+  return (
+    <>
+      <div className="dashboard-Container">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-md-8 col-lg-8">
+              <div className="card p-3">
+                <h3 className="mb-3 text-center">Setting</h3>
+                <form>
+                  <div className="key-block">
+                    <label className="form-label font-700">Web-Hook</label>
+                    <div className="api-key">
+                      <span>acjbsvbffvbubvsbvjdsbvdbf161456456svdjshbvjh</span>
+                      <button type="button" className="btn btn-primary">
+                        <IconCopy />
+                      </button>
+                    </div>
+                  </div>
+
+                  <div className="img-upload-block">
+                    <div className="form-group mb-0">
+                      <label className="form-label font-700">Historical Data</label>
+                      <div className="user-img-block d-flex justify-content-center">
+                        <img src={ImgFile} alt="" />
+                      </div>
+                      <div className="upload-btn ">
+                        <input className="form-control" id="Choose a file" name="file" type="file" />
+                        <button type="button" className="btn btn-primary w-100">
+                          Upload a file
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Settings;
