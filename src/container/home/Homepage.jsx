39ca75af89576b976339ccfb47362cd4d3b2fed6
bhavinmakavana <bhavin@thinkwik.com>
2023-09-02 12:58:37 +0530

    -Themecolor Set

--- src/container/home/Homepage.jsx
+++ src/container/home/Homepage.jsx
@@ -1,5 +1,34 @@
+import ImgLogo from "../../assets/images/logo.svg";
+
 const HomePage = () => {
-    return <p>Home Page</p>
-}
+  return (
+    <>
+      <div className="app-wrapper landing-page">
+        <div className="app-content">
+          <div className="landing-header">
+            <div className="container">
+              <div className="nav-logo">
+                <a className="navbar-brand">
+                  <img src={ImgLogo} alt="" />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="landing-banner">
+            <div className="container">
+              <div className="row align-content-center">
+                <div className="col-md-6 col-lg-6">
+                  <h1>Hello</h1>
+                </div>
+                <div className="col-md-6 col-lg-6"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default HomePage;
